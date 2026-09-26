2dbea3b90965dc69da5585d6e7d0bf9efe9cc379
akashk21149-maker <akashk21149@gmail.com>
2026-09-26 22:00:02 +0530

    Submitted on date 26/09/2026 by Akash

---
+++ src/Arrays/BubbleSort/Demo.java
@@ -0,0 +1,25 @@
+package Arrays.BubbleSort;
+
+public class Demo {
+    public static void bubbleSort(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+    public static void printArr(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {2,5,3,55,4,8};
+        bubbleSort(arr);
+        printArr(arr);
+    }
+}
